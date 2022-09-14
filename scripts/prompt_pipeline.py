ae0ac1af0f0b95dd1166c3e19abe73dd9449254f
Natnael Melese <natrix277@gmail.com>
2022-09-14 17:44:59 -0400

    Response parsing and pipeline

--- scripts/prompt_pipeline.py
+++ scripts/prompt_pipeline.py
@@ -4,6 +4,87 @@ import os
 sys.path.append(os.path.abspath(os.path.join("./scripts/")))
 from logger import logger
 class PromptPipeline():
+    def __init__(self, currernt_technique='technique1') -> None:
+        self.techniques_list = ['technique1']
+        self.currernt_technique = currernt_technique
+
+    def send_request_to_cohere(self, co, prompt):
+        """
+            Sends an API request to cohere and reuturns the response
+        """
+        response = co.generate( 
+            model='xlarge', 
+            prompt=prompt, 
+            max_tokens=400, 
+            temperature=0.5, 
+            k=0, 
+            p=1, 
+            frequency_penalty=0, 
+            presence_penalty=0, 
+            stop_sequences=["--"], 
+            return_likelihoods='NONE')
+        
+        return response.generations[0].text
+
+    def get_tokens(self, data):
+        """
+            Returns dictionary form of tokens array in the json
+            example:
+                "tokens": [
+                    {
+                        "text": "Bachelor",
+                        "entityLabel": "DIPLOMA"
+                    },
+                    {
+                        "text": "Mechanical Engineering",
+                        "entityLabel": "DIPLOMA_MAJOR"
+                    },
+                    {
+                        "text": "Physical Science",
+                        "entityLabel": "DIPLOMA_MAJOR"
+                    },
+                    {
+                        "text": "3+ years",
+                        "entityLabel": "EXPERIENCE"
+                    },
+                    {
+                        "text": "developing",
+                        "entityLabel": "SKILLS"
+                    },
+                    {
+                        "text": "fiber optic cables",
+                        "entityLabel": "SKILLS"
+                    },
+                    {
+                        "text": "connector related products",
+                        "entityLabel": "SKILLS"
+                    }
+
+                    in to
+                    {
+                        "DIPLOMA" : "Bachelor",
+                        "DIPLOMA_MAJOR":"Mechanical Engineering, Physical Science",
+                        "EXPERIENCE":"3+ years",
+                        "SKILLS":"developing, fiber optic cables, connector related products"
+                    }
+        """
+        token_dict = {}
+        try:
+            for token in data['tokens']:
+                entity_label = token['entityLabel']
+                if entity_label in token_dict.keys():
+                    token_dict[entity_label] += f",{token['text']}"
+                else:
+                    token_dict[entity_label] = f"{token['text']}"
+            logger.info("Tokens converted to Dict")
+        except Exception as e:
+            logger.error(e)
+        return token_dict
+
+    def process_response(response):
+        response = str(response).strip()
+
+
     def extract_values(self, data):
         """
             We will extract the values from each json dictionaries and
@@ -22,10 +103,11 @@ class PromptPipeline():
             EXPERIENCE: 3+ years
             SKILLS: developing, fiber optic cables, connector related products
         """
-        s: str = 'Job Description: '
+        s: str = ''
         try:
             for d in data:
-                s+=d['text']
+                s+='Job Description: '
+                s+=d['document']
                 s+='\n'
                 """
                     Extract entities from tokens
@@ -37,10 +119,13 @@ class PromptPipeline():
                 """
                 token_dict = {}
                 for token in d['tokens']:
-                    if token['entityLabel'] in token_dict.keys():
-                        token_dict['entityLabel']+=f",{token['text']}"
+                    entity_label = token['entityLabel']
+                    if entity_label in token_dict.keys():
+                        token_dict[entity_label] += f",{token['text']}"
+                    else:
+                        token_dict[entity_label] = f"{token['text']}"
                 dict_str = ''
-                for key,value in token_dict.iteritems():
+                for key,value in token_dict.items():
                     dict_str+=f"{key}: {value}\n"
                 s+=dict_str
                 s+='\n--\n'
@@ -49,6 +134,6 @@ class PromptPipeline():
         except Exception as e:
             logger.error(e)
     
-    
+
 
 
