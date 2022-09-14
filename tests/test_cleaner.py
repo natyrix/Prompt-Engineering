95819e160a61167d7dd2fa35304b17275c63d79c
Natnael Melese <natrix277@gmail.com>
2022-09-14 19:38:55 -0400

    CI and test files intialized

---
+++ tests/test_cleaner.py
@@ -0,0 +1,13 @@
+import unittest
+import pandas as pd
+import sys
+import os
+
+
+class TestDataClean(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
