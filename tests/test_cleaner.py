2b33609c009323658857a786c1fe42b348621015
Natnael Melese <natrix277@gmail.com>
2022-09-14 20:52:54 -0400

    Remove irrelevant Columns Tested

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -2,12 +2,24 @@ import unittest
 import pandas as pd
 import sys
 import os
+import json
 
 
-# class TestDataClean(unittest.TestCase):
-#     def setUp(self) -> None:
-#         pass
+#Prompt-Engineering
+sys.path.append(os.path.abspath(os.path.join("../Prompt-Engineering/")))
+
+from scripts import data_cleaning_pipeline
+
+class TestDataClean(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cleaner = data_cleaning_pipeline.DataCleaningPipeline()
+        with open('./tests/sample.json', 'r') as f:
+            self.data = json.loads(f.read())
+
+    def test_remove_irrelevant_pairs(self):
+        self.cleaner.remove_irrelevant_pairs(self.data, 'relations')
+        self.assertEqual(list(self.data.keys()), ['document', 'tokens'])
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
